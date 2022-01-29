8cba2bdab6b371efebd4bd2b7b666662efaff347
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-01-29 11:27:21 -0500

    watch tp and shortcodes and first tp with args

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php get_header();?>
+<?php 
+$tpPath = "src/template-parts/";
+$shortcodesPath = "src/shortcodes/";
+get_header();?>
 
-solo body
-
+<?php 
+get_template_part( $tpPath.'test', null, array( 
+  'class' => 'featured-home',
+  'data'  => array(
+    'size' => 'large',
+    'is-active' => true,
+  )) 
+);
+?>
 <?php get_footer();?>
---
+++ src/template-parts/test.php
@@ -0,0 +1,1 @@
+<?php echo $args['data']['size'];?>
