3a833a1cd958cf1ce4e54fd00dc6384aa1d8eb5a
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-01-29 00:29:41 -0500

    js hint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const { src, dest, watch } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const gulp   = require('gulp')
+const jshint = require('gulp-jshint')
 const SASS_DIR = "src/scss/**/*.scss"
-const SASS_DIR = "src/scss/**/*.scss"
+const JS_DIR = "src/js/**/*.js"
 function compileSass(done) {
   src(SASS_DIR)
   .pipe(sass().on('error', sass.logError))
@@ -11,8 +13,15 @@ function compileSass(done) {
 function watchSass() {
   watch(SASS_DIR, compileSass);
 }
+function jsHint(cb) {
+  src(JS_DIR)
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'))
+  cb()
+}
 
 
 
 exports.compileSass = compileSass
 exports.watchSass = watchSass
+exports.jsHint = jsHint
