0b29c948610a3152284e7f3a17945cf1e574e7ae
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-01-28 22:45:21 -0500

    add watch task gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
-'use strict'
-const { src, dest } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const SASS_DIR = "src/scss/**/*.scss"
 function compileSass(done) {
-  src('src/scss/**/*.scss')
+  src(SASS_DIR)
   .pipe(sass().on('error', sass.logError))
-  .pipe(dest('.'));
- done();
+  .pipe(dest('.'))
+ done()
 }
+function watchSass() {
+  watch(SASS_DIR, compileSass);
+}
+
+
 exports.compileSass = compileSass
+exports.watchSass = watchSass
