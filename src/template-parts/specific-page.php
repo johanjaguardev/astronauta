79b8be4866b013f0e6b41400b126d2bb3763bc01
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-01-29 11:49:30 -0500

    custom field in page

--- src/template-parts/specific-page.php
+++ src/template-parts/specific-page.php
@@ -1,8 +1,9 @@
 <?php 
 $page = get_page_by_path( $args['page'] );
+
 echo get_the_title( $page );
 echo get_the_content( $page );
-//echo get_post_meta( $page['ID'], 'link', TRUE);
+echo get_post_meta( $page->ID, 'link', TRUE);
 echo get_the_post_thumbnail( $page );
 
 ?>
