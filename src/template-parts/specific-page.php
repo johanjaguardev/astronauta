d996e73fe94e3a3199f1fa7ce32d18dda50ddb24
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-01-30 23:03:35 -0500

    home page and css

--- src/template-parts/specific-page.php
+++ src/template-parts/specific-page.php
@@ -1,8 +1,27 @@
 <?php 
 $page = get_page_by_path( $args['page'] );
 
-echo get_the_title( $page );
-echo get_the_content( $page );
-echo get_post_meta( $page->ID, 'link', TRUE);
-echo get_the_post_thumbnail( $page );
+$title = get_the_title( $page );
+$content= get_the_content( $page->ID );
+$link= get_post_meta( $page->ID, 'btn-link', TRUE);
+$label= get_post_meta( $page->ID, 'btn-label', TRUE);
+$image = get_the_post_thumbnail( $page->ID );
 ?>
+
+<div class="<?php echo $args['page']?>__page page">
+  <div class="<?php echo $args['page']?>__container container">
+    <div class="<?php echo $args['page']?>__page-left page__left">
+      <h2><?php echo $title?></h2>
+      <div class="<?php echo $args['page']?>__page-content page__content">
+        <?php echo $content?>
+      </div>
+      <a href="<?php echo $link?>" class="home__page-link page__link btn"><?php echo $label?></a>
+
+    </div>
+    <div class="<?php echo $args['page']?>__page-right page__right">
+      <figure class="<?php echo $args['page']?>__page-img page__img">
+      <?php echo $image;?>  
+      </figure>
+    </div>
+  </div>
+</div> 
