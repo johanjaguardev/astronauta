cd15701498d69cdff500bed54ec2316b2f5e9c65
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-01-31 00:22:08 -0500

    fix hamburguer js error and fix content specific page

--- src/js/hamburguer.js
+++ src/js/hamburguer.js
@@ -1,6 +1,8 @@
 const hamburguer = document.querySelector('.hamburguer')
 const header = document.querySelector('.header')
-hamburguer.addEventListener('click', () => {
-  hamburguer.classList.toggle('open')
-  header.classList.toggle('open')
-})
+if(hamburguer) {
+  hamburguer.addEventListener('click', () => {
+    hamburguer.classList.toggle('open')
+    header.classList.toggle('open')
+  })
+}
--- src/template-parts/specific-page.php
+++ src/template-parts/specific-page.php
@@ -1,25 +1,34 @@
 <?php 
-$page = get_page_by_path( $args['page'] );
+/** * Values given like parameters */
+$slug= $args['page'];
+$columns= $args['columns'];
+$imageBg= $args['imageBg'];
 
-$title = get_the_title( $page );
-$content= get_the_content( $page->ID );
+/** * Values given from the specific page */
+$page= get_page_by_path($slug);
+$title= get_the_title($page);
+
+$content = $page->post_content;
+$content = apply_filters('the_content', $content);
+$content = str_replace(']]>', ']]>', $content);
+
 $link= get_post_meta( $page->ID, 'btn-link', TRUE);
 $label= get_post_meta( $page->ID, 'btn-label', TRUE);
 $image = get_the_post_thumbnail( $page->ID );
 ?>
 
-<div class="<?php echo $args['page']?>__page page">
-  <div class="<?php echo $args['page']?>__container container">
-    <div class="<?php echo $args['page']?>__page-left page__left">
-      <h2><?php echo $title?></h2>
-      <div class="<?php echo $args['page']?>__page-content page__content">
-        <?php echo $content?>
+<div class="<?php echo $slug;?>__page page">
+  <div class="<?php echo $slug;?>__container container">
+    <div class="<?php echo $slug;?>__page-left page__left">
+      <h2><?php echo $title;?></h2>
+      <div class="<?php echo $slug;?>__page-content page__content">
+        <?php echo $content;?>
       </div>
       <a href="<?php echo $link?>" class="home__page-link page__link btn"><?php echo $label?></a>
 
     </div>
-    <div class="<?php echo $args['page']?>__page-right page__right">
-      <figure class="<?php echo $args['page']?>__page-img page__img">
+    <div class="<?php echo $slug;?>__page-right page__right">
+      <figure class="<?php echo $slug;?>__page-img page__img">
       <?php echo $image;?>  
       </figure>
     </div>
