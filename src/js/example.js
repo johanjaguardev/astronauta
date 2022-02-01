fd3a50299086f2bf5556fc9f598d784e68828067
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-02-01 13:12:17 -0500

    hero home and objetives responsive tablet

--- src/js/example.js
+++
@@ -1,1 +0,0 @@
-console.log('otro test para el watch')
--- src/js/hamburguer.js
+++ src/js/hamburguer.js
@@ -1,8 +1,21 @@
 const hamburguer = document.querySelector('.hamburguer')
 const header = document.querySelector('.header')
+const menuLinks = document.querySelectorAll('.header__menu li a')
+
+console.log(menuLinks)
+
 if(hamburguer) {
   hamburguer.addEventListener('click', () => {
     hamburguer.classList.toggle('open')
     header.classList.toggle('open')
+  })
+}
+
+if( menuLinks ) {
+  menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      hamburguer.classList.toggle('open')
+      header.classList.toggle('open')
+    })
   })
 }
